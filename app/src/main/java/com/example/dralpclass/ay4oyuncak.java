59884c47378d8b457f777e1d97e9ac4994276077
tunclberkay <tunc.45berkay@gmail.com>
2022-09-20 15:33:19 +0300

    yeni aylar

---
+++ app/src/main/java/com/example/dralpclass/ay4oyuncak.java
@@ -0,0 +1,14 @@
+package com.example.dralpclass;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ay4oyuncak extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ay4oyuncak);
+    }
+}
---
+++ app/src/main/java/com/example/dralpclass/ay8oyuncak.java
@@ -0,0 +1,14 @@
+package com.example.dralpclass;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ay8oyuncak extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ay8oyuncak);
+    }
+}
