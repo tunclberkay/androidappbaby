5bb65015bb9c10d8bf8fc0610e673e95a5981252
tunclberkay <tunc.45berkay@gmail.com>
2022-09-30 22:25:18 +0300

    geri kalanlar 30 eylül

--- app/src/main/java/com/example/dralpclass/ay4beslenme.java
+++ app/src/main/java/com/example/dralpclass/ay4beslenme.java
@@ -1,8 +1,14 @@
 package com.example.dralpclass;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ay4beslenme extends AppCompatActivity {
 
@@ -10,5 +16,45 @@ public class ay4beslenme extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ay4beslenme);
+
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setSelectedItemId(R.id.home);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                switch (menuItem.getItemId()){
+                    case R.id.dashbourd:
+                        startActivity(new Intent(getApplicationContext()
+                                ,Dashbourd.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext()
+                                ,ay1dashbourd.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.about:
+                        startActivity(new Intent(getApplicationContext()
+                                ,Dashbourd2.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+
+                }
+                return false;
+            }
+        });
+
+
+    }
+    public void foto9(View view){
+        Intent ıntent = new Intent(getApplicationContext(), ay1dashbourd.class);
+        startActivity(ıntent);
+
+
     }
 }
