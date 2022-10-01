f87d3d0670ffa25d0bcebf78a140bb7873544a0f
tunclberkay <tunc.45berkay@gmail.com>
2022-10-01 18:13:23 +0300

    sık sorulanlar

--- app/src/main/java/com/example/dralpclass/Dashbourd.java
+++ app/src/main/java/com/example/dralpclass/Dashbourd.java
@@ -6,8 +6,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.Random;
 
 public class Dashbourd extends AppCompatActivity {
 
@@ -16,35 +20,41 @@ public class Dashbourd extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashbourd);
 
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
-
-        bottomNavigationView.setSelectedItemId(R.id.dashbourd);
-
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+        Button myButton = (Button) findViewById(R.id.button2);
+        myButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            public void onClick(View v) {
+                // Here, we are generating a random number
+                Random generator = new Random();
+                int number = generator.nextInt(5) + 1;
+                // The '5' is the number of activities
 
-                switch (menuItem.getItemId()){
-                    case R.id.dashbourd:
-                        return true;
-                    case R.id.home:
-                        startActivity(new Intent(getApplicationContext()
-                                ,MainActivity.class));
-                        overridePendingTransition(0,0);
-                        return true;
-                    case R.id.about:
-                        startActivity(new Intent(getApplicationContext()
-                                ,Dashbourd2.class));
-                        overridePendingTransition(0,0);
-                        return true;
+                Class activity = null;
 
-
+                // Here, we are checking to see what the output of the random was
+                switch (number) {
+                    case 1:
+                        // E.g., if the output is 1, the activity we will open is ActivityOne.class
+                        activity = ay5asi.class;
+                        break;
+                    case 2:
+                        activity = ay2oyuncak.class;
+                        break;
+                    case 3:
+                        activity = ay8beslenme.class;
+                        break;
+                    case 4:
+                        activity = hakkimda.class;
+                        break;
+                    default:
+                        activity = ay7asi.class;
+                        break;
                 }
-                return false;
+                // We use intents to start activities
+                Intent intent = new Intent(getBaseContext(), activity);
+                startActivity(intent);
             }
         });
-
-
 
     }
 }
